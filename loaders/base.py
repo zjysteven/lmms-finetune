7d417ecd061e9cf4361402b8ae6a568a043693b8
Jingyang <jingyang.zhang@duke.edu>
2024-08-27 18:45:29 -0400

    add a new argument for finetuning from local ckpt

--- loaders/base.py
+++ loaders/base.py
@@ -8,13 +8,15 @@ from transformers import PreTrainedModel, PreTrainedTokenizer, AutoProcessor, Bi
 class BaseModelLoader(ABC):
     def __init__(
         self, 
-        model_hf_path: str, 
+        model_hf_path: str,
+        model_local_path: str, 
         compute_dtype: torch.dtype,
         bnb_config: Optional[BitsAndBytesConfig] = None,
         use_flash_attn: bool = False,
         device_map: Optional[Union[Dict, str]] = None,
     ) -> None:
         self.model_hf_path = model_hf_path
+        self.model_local_path = model_local_path
         self.loading_kwargs = dict(
             torch_dtype=compute_dtype,
             quantization_config=bnb_config,

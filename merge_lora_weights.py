d42136b2164e2a08f2d42a86692feff50a4055b8
lavinal712 <austin712@mail.ustc.edu.cn>
2024-12-12 02:30:44 +0000

    add llama-3.2-vision doc; add lora merge script and doc

--- merge_lora_weights.py
+++ merge_lora_weights.py
@@ -12,6 +12,7 @@ def merge_lora(args):
     model_hf_path = MODEL_HF_PATH[args.model_id]
     model_local_path = model_local_path if args.model_local_path else model_hf_path
     model_family_id = MODEL_FAMILIES[args.model_id]
+    model_save_path = args.model_save_path if args.model_save_path else args.model_path
     
     bnb_config = None
     if args.load_4bit:
@@ -30,17 +31,19 @@ def merge_lora(args):
         bnb_config=bnb_config,
         device_map="cpu",
     )
-    model, tokenizer, processor, config = loader.load()
+    model, tokenizer, processor, config = loader.load(args.load_model)
+    print(args.load_model)
     
-    print("Loading LoRA weights...")
-    model = PeftModel.from_pretrained(model, args.model_path)
-    print("Merging LoRA weights...")
-    model = model.merge_and_unload()
-    print("Model is loaded...")
-
-    model.save_pretrained(args.save_model_path)
-    tokenizer.save_pretrained(args.save_model_path)
-    processor.save_pretrained(args.save_model_path)
+    if args.load_model:
+        print("Loading LoRA weights...")
+        model = PeftModel.from_pretrained(model, args.model_path)
+        print("Merging LoRA weights...")
+        model = model.merge_and_unload()
+        print("Model is loaded...")
+        model.save_pretrained(model_save_path)
+    
+    tokenizer.save_pretrained(model_save_path)
+    processor.save_pretrained(model_save_path)
 
 
 if __name__ == "__main__":
@@ -48,8 +51,9 @@ if __name__ == "__main__":
     parser.add_argument("--model_id", type=str, required=True)
     parser.add_argument("--model_local_path", type=str, default="")
     parser.add_argument("--model_path", type=str, required=True)
-    parser.add_argument("--load_4bit", type=bool, default=False)
-    parser.add_argument("--save_model_path", type=str, required=True)
+    parser.add_argument("--model_save_path", type=str, default="")
+    parser.add_argument("--load_model", action="store_true")
+    parser.add_argument("--load_4bit", action="store_true")
 
     args = parser.parse_args()
 

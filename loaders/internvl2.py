2d0b6cc2dd7c17956c03e999db79a2d6768ab88a
lavinal712 <austin712@mail.ustc.edu.cn>
2025-11-22 12:17:54 +0800

    add internvl2 loader

---
+++ loaders/internvl2.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+from transformers import AutoProcessor, AutoModel, AutoTokenizer, PreTrainedTokenizer, AutoConfig
+
+from . import register_loader
+from .base import BaseModelLoader
+
+
+@register_loader("internvl2")
+class InternVL2ModelLoader(BaseModelLoader):
+    def load(self, load_model: bool = True) -> Tuple[AutoModel, PreTrainedTokenizer, AutoProcessor, AutoConfig]:
+        if load_model:
+            model = AutoModel.from_pretrained(
+                self.model_local_path,
+                trust_remote_code=True,
+                **self.loading_kwargs,
+            )
+            model.config.hidden_size = model.config.llm_config.hidden_size # useful for deepspeed
+        else:
+            model = None
+
+        processor = AutoProcessor.from_pretrained(self.model_hf_path, trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(self.model_hf_path, trust_remote_code=True)
+        config = AutoConfig.from_pretrained(self.model_local_path, trust_remote_code=True)
+        return model, tokenizer, processor, config
+
+
+if __name__ == "__main__":
+    import torch
+    loader = InternVL2ModelLoader(
+        model_hf_path="OpenGVLab/InternVL2-8B",
+        model_local_path="/aiarena/group/gmgroup/hongyq/models/OpenGVLab/InternVL2-8B",
+        compute_dtype=torch.bfloat16,
+    )
+    model, tokenizer, processor, config = loader.load()
